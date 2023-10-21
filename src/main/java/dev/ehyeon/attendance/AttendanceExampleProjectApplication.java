f9a991dfb17090bc2cddd79b42c2f35694df2f52
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-10-21 16:13:07 +0900

    init: init project

---
+++ src/main/java/dev/ehyeon/attendance/AttendanceExampleProjectApplication.java
@@ -0,0 +1,12 @@
+package dev.ehyeon.attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceExampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceExampleProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/dev/ehyeon/attendance/AttendanceExampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ehyeon.attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceExampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
