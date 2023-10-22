f5e4ebeab62ae7c30c52e823710ed78702dc363f
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-10-22 15:21:39 +0900

    feat: post 유효성 검증에 실패하면 기본 예외 응답 대신 ErrorResponse 전달

--- src/main/java/dev/ehyeon/attendance/global/error/Error.java
+++ src/main/java/dev/ehyeon/attendance/global/error/Error.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum Error {
 
+    BAD_REQUEST(400, "Bad Request"),
+
     DUPLICATED_ID(409, "Duplicated Id"),
     USER_NOT_FOUND(404, "User Not Found");
 
